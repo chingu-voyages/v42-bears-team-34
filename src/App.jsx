c7af4d5fa647622794474854411b38ee942da5c7
David Eastmond <davebuck1981@gmail.com>
2023-01-15 18:57:04 -0500

    update folder structure, remove NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Home from "./Pages/Home/Home";
-import Blog from "./Pages/Blog/Blog";
-import Contact from "./Pages/Contact/Contact";
-import Login from "./Pages/Login/Login";
-import Signup from "./Pages/Signup/Signup";
+import Home from "./pages/home/Home";
+import Blog from "./pages/blog/Blog";
+import Contact from "./pages/contact/Contact";
+
+import Signup from "./pages/signup/Signup";
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { LoginPage } from './pages/login';
 
 
 function App() {
-
   return (
     <>
     <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
     </div>
     </>
   );
