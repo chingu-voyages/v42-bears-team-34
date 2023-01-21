12715d9d9b512175e919c8d606db6d77600bce27
David Eastmond <davebuck1981@gmail.com>
2023-01-21 17:12:42 -0500

    token manager

---
+++ src/services/token-manager/token-manager.js
@@ -0,0 +1,23 @@
+/**
+ * getting and writing the JWT token ("auth-token") to sessionStorage
+ */
+
+const KEY = "auth-token"
+export class TokenManger {
+  /**
+   * @returns {string | null }
+   */
+  static getToken () {
+    return sessionStorage.getItem(KEY)
+  }
+
+  /**
+   * Simply writes `auth-token` data to sessionStorage
+   * @param { string } data 
+   * @returns { null }
+   */
+  static writeToken (data) {
+    if (data === undefined || data === null) console.warn("TokenManager: Writing null or undefined data to auth-token")
+    sessionStorage.setItem(KEY, data)
+  }
+}
