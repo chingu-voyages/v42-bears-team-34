2e67e67251b23ae8bb746d1c891963d9693af2a0
Bachir <bachir@Bachirs-MacBook-Pro.local>
2023-01-23 21:58:59 -0500

    removed unnecessary comments codes from signup page

--- src/pages/signup/SignupPage.jsx
+++ src/pages/signup/SignupPage.jsx
@@ -12,7 +12,6 @@ import { useState } from 'react';
 import { Box } from '@mui/system';
 import Typography from '@mui/material/Typography';
 
-
 const steps = [
   "Your personal information", 
   "Create your password", 
@@ -38,7 +37,6 @@ function SignupPage() {
       newSkipped = new Set(newSkipped.values());
       newSkipped.delete(activeStep);
     }
-
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
     setSkipped(newSkipped);
   };
@@ -49,8 +47,6 @@ function SignupPage() {
 
   const handleSkip = () => {
     if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
       throw new Error("You can't skip a step that isn't optional.");
     }
 
@@ -62,21 +58,13 @@ function SignupPage() {
     });
   };
 
-  // const handleReset = () => {
-  //   setActiveStep(0);
-  // };
-
   return (
     <Box sx={{ width: '50%', margin: 'auto' }}>
       <Stepper activeStep={activeStep} alternativeLabel>
         {steps.map((label, index) => {
           const stepProps = {};
           const labelProps = {};
-          // if (isStepOptional(index)) {
-          //   labelProps.optional = (
-          //     <Typography variant="caption">Optional</Typography>
-          //   );
-          // }
+
           if (isStepSkipped(index)) {
             stepProps.completed = false;
           }
@@ -89,12 +77,8 @@ function SignupPage() {
       </Stepper>
       {activeStep === steps.length ? (
         <React.Fragment>
-          {/* <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography> */}
           <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
             <Box sx={{ flex: '1 1 auto' }} />
-            {/* <Button onClick={handleReset}>Reset</Button> */}
           </Box>
         </React.Fragment>
       ) : (
