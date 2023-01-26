ca132e8f8274c03e198f54aab97e9d6133f47f7c
David Eastmond <davebuck1981@gmail.com>
2023-01-26 08:30:10 -0500

    plaid link token client

---
+++ src/services/api-clients/plaid-client.js
@@ -0,0 +1,23 @@
+import { BaseClient } from "./base-client";
+
+/**
+ * This client gets the link token we need to
+ * open up link. This requires the user to be authenticated
+ * with a JWT from our API
+ */
+export class PlaidClient extends BaseClient {
+  constructor({ authToken }) {
+    super({
+      "Authorization": `Bearer ${authToken}`,
+    });
+  }
+
+  /**
+   * Send request to our API for the link token
+   * @param {string} authenticationToken
+   * @returns {Promise<{ expiration: ISODateString, link_token: string, request_id: string}>}
+   */
+  async getLinkToken () {
+    return super.getData("/plaid/get_token");
+  }
+}
