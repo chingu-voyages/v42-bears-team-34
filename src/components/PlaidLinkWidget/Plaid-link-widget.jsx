026328df2ff3536749f7105036e48a8ec5bb342a
David Eastmond <davebuck1981@gmail.com>
2023-01-28 17:47:38 -0500

    linkWidget continued work

--- src/components/PlaidLinkWidget/Plaid-link-widget.jsx
+++ src/components/PlaidLinkWidget/Plaid-link-widget.jsx
@@ -1,24 +1,40 @@
 
 import { Button, styled } from "@mui/material";
-import { useCallback } from "react";
+import { useCallback, useContext } from "react";
 import { usePlaidLink } from "react-plaid-link";
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import { PlaidClient } from "../../services/api-clients/plaid-client";
 import { TokenManager } from "../../services/token-manager/token-manager";
-
+import AppContext from "../../context/AppContext";
+import { APP_ACTIONS } from "../../context/app.actions";
 const StyledCustomPlaidButton = styled(Button)((props) => ({
   "&.MuiButtonBase-root": {
     "border-radius": props.borderRadius || "30px"
   }
 }))
 
+const clearLinkTokenError = () => {
+  return {
+    error_type: "",
+    error_code: "",
+    error_message: ""
+  }
+},
 /**
- * The button should be available if authentication and linkToken work
- * props should contain link token
- * @param {{ linkToken: string, onLinkButtonClicked: () => void }} props
+ * This component only handles getting the plaid widget to work and to exchange tokens.
+ * It won't fetch trigger a fetching of financial data. Do that outside of this component.
+ * The button should be enabled (available) if authentication and linkToken work.
+ * Props should contain link token
+ * @param {{ linkToken: string, onLinkButtonClicked: () => void, onPlaidSuccessComplete: () => void }} props
  * @returns 
  */
 function PlaidLinkWidget (props) {
+  const { dispatch } = useContext(AppContext);
+
+  // This is called when the plaid token exchange is complete to signal to parent component.
+  const handlePlaidSuccessComplete = () => {
+    props.onPlaidSuccessComplete && props.onPlaidSuccessComplete();
+  };
 
   const onSuccess = useCallback((public_token, metaData) => {
     const exchangePublicTokenForAccessToken =  async() => {
@@ -27,22 +43,44 @@ function PlaidLinkWidget (props) {
       // Get token from sessionStorage
       const jwtToken = TokenManager.getToken();
       const plaidClient = new PlaidClient({ authToken: jwtToken });
-      const response = await plaidClient.postSetPublicToken(public_token);
-      console.log("**28 exchangePublicTokenForAccessToken", response)
-      // We should handle the data in some kind of state?
+
+      // Send the request to exchange the public token for a link token
+      await plaidClient.postSetPublicToken(public_token);
+
+      dispatch({
+        type: APP_ACTIONS.SET_STATE,
+        state: { linkSuccess: true, hasLinkTokenError: false, linkTokenError: clearLinkTokenError() }
+      });
+      handlePlaidSuccessComplete();
     }
     exchangePublicTokenForAccessToken();
   }, [])
 
   // Error handling if user exits Link without connecting or otherwise some other error
   const onExit = useCallback((err, metadata) => {
+    // Dispatch error state
+    const { 
+      error_type,
+      error_code,
+      error_message
+    } = err;
+    dispatch({
+      type: APP_ACTIONS.SET_STATE,
+      state: {
+        linkSuccess: false,
+        hasLinkTokenError: true,
+        linkTokenError: {
+          error_type,
+          error_code,
+          error_message
+        }
+      }
+    })
     // Do something
   }, [])
 
   const onEvent = useCallback((eventName, metaData) => {
-    // Log data
-    console.info(eventName);
-    console.info(metaData)
+    // Log something
   })
   const handleLinkButtonClicked = useCallback((open) => {
     props.onLinkButtonClicked && props.onLinkButtonClicked()
@@ -59,8 +97,6 @@ function PlaidLinkWidget (props) {
   }
 
   if (window.location.href.includes("?oauth_state_id=")) {
-    // TODO: figure out how to delete this ts-ignore
-    // @ts-ignore
     plaidConfig.receivedRedirectUri = window.location.href;
     isOauth = true;
   }
@@ -69,7 +105,7 @@ function PlaidLinkWidget (props) {
     // Plaid button
     <StyledCustomPlaidButton 
       type="button"
-      onClick={() => handleLinkButtonClicked(open)} // Open is a function
+      onClick={() => handleLinkButtonClicked(open)}
       disabled={!ready}
       variant="contained"
       startIcon={<AccountBalanceIcon />}
