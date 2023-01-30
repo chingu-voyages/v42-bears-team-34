846234775546e9e1cdb43ec956ec4b290df12722
David Eastmond <davebuck1981@gmail.com>
2023-01-29 19:22:19 -0500

    placeholder pages for user and admin

---
+++ src/pages/admin/ApplicationsPage.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+function ApplicationsPage (props) {
+  return (
+    <Box>
+      <h1>Applications</h1>
+    </Box>
+  )
+}
+
+export default ApplicationsPage;
