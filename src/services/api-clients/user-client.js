1040f94b34e3f0563f61d5965fb1c40cebdcfdfd
David Eastmond <davebuck1981@gmail.com>
2023-01-29 19:20:35 -0500

    create user client

---
+++ src/services/api-clients/user-client.js
@@ -0,0 +1,22 @@
+import { BaseClient } from "./base-client";
+
+/**
+ * This class should handle communication to our backend
+ * for users that have a JWT
+ */
+export class UserClient extends BaseClient {
+  constructor({ authToken }) {
+    super(
+      {
+        "Authorization": `Bearer ${authToken}`,
+      }
+    )
+  }
+   /**
+   * This returns the profile of the requesting authenticated user. They need to have a JWT in session
+   * @returns {Promise<{ id: string, firstName: string, lastName: string, email: string, role: "user" | "admin", iat: number, exp: number, expired?: boolean }>}
+   */
+   async getUserProfile() {
+    return super.getData("/auth/profile")
+  }
+}
