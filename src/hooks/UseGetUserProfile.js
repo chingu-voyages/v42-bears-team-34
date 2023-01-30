2655265a9954ebbf4e124ba76dc70be1e2a460a6
David Eastmond <davebuck1981@gmail.com>
2023-01-29 19:22:32 -0500

    get user profile hook

---
+++ src/hooks/UseGetUserProfile.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import { UserClient } from "../services/api-clients/user-client";
+
+const useGetUserProfile = (authToken) => {
+  const [userProfile, setUserProfile] = useState(null);
+  useEffect(() => {
+    const getAuth = async() => {
+      const userClient = new UserClient({ authToken });
+      const data = await userClient.getUserProfile();
+      setUserProfile(data);
+    }
+    getAuth();
+  },[authToken])
+
+  return [userProfile];
+}
+
+export default useGetUserProfile;
