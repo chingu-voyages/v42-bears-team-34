e9d25d0c93daddb58131b1f0f88573f71df7b2fe
David Eastmond <davebuck1981@gmail.com>
2023-01-29 19:22:04 -0500

    create user and admin react-router routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { LoginPage } from './pages/login';
+
 import Home from "./pages/home/Home";
 import Blog from "./pages/blog/Blog";
 import Contact from "./pages/contact/Contact";
-import Signup from "./pages/signup/SignupPage";
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import { LoginPage } from './pages/login';
+import Signup from "./pages/signup/Signup";
 import NavBar from './components/NavBarComponent/NavBar'
+import './App.css'
+import { AdminApplicationsPage } from './pages/admin';
+import { RouteProtector } from './components/RouteProtector/RouteProtector';
+import { UserPage } from './pages/user';
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<Signup />} />
-        </Routes>
-      </div>
-    </>
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/admin/login" element={<LoginPage isAdmin={true} />} />
+        <Route 
+          path="/admin/applications" 
+          element={
+            <RouteProtector admin={true} redirectionComponent={<LoginPage isAdmin={true} />}>
+              <AdminApplicationsPage />
+            </RouteProtector>
+          } 
+        />
+        <Route 
+          path="/user/applications" 
+          element={
+          <RouteProtector requiresAuth={true} redirectionComponent={ <LoginPage />} >
+              <UserPage />
+            </RouteProtector>
+          } 
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
   );
 }
 
