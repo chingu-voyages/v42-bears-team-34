05bd6ec3d31d103e4b71b995aa78a9a7af3f00e1
David Eastmond <davebuck1981@gmail.com>
2023-02-04 01:32:05 -0500

    import confirmation modal

---
+++ src/components/SignupModal/SignupModal.jsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Box, Button, FormGroup, FormControlLabel, Typography, Modal } from '@mui/material';
+import StyledButton from '../StyledButton/StyledButton';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+function SignupModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Start the process
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Check if you are eligible or call us at 999-999-9999
+          </Typography>
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="I am a Canadian resident" />
+            <FormControlLabel control={<Checkbox />} label="I am 18 years old or older" />
+            <FormControlLabel control={<Checkbox />} label="I have a minimum income of 1000$ (per month)" />
+            <FormControlLabel control={<Checkbox />} label="I am not in bankruptcy proceedings" />
+          </FormGroup>
+          <Box>
+            <StyledButton label={"I agree"} />
+            <StyledButton label={"Cancel"} />
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default SignupModal;
