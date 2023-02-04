25200a5f010cb213e6021797cd2db3d1b5f1ef93
David Eastmond <davebuck1981@gmail.com>
2023-02-04 01:20:58 -0500

    created enums for signup fields

---
+++ src/pages/signup/sign-up-fields.js
@@ -0,0 +1,19 @@
+export const SIGNUP_FIELDS = {
+  firstName: "firstName",
+  lastName: "lastName",
+  streetAddress: "streetAddress",
+  city: "city",
+  province: "province",
+  postalCode: "postalCode",
+  phone: "phone",
+
+  email: "email",
+  password1: "password1",
+  password2: "password2",
+
+  applicantIncome: "applicantIncome",
+  applicantGender: "applicantGender",
+  applicantOccupation: "applicantOccupation",
+  requestedLoanAmount: "requestedLoanAmount",
+  loanPurpose: "loanPurpose"
+}
