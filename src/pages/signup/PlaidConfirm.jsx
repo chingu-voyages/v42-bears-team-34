096f250a31e5bad8ae571a7b8e09348dbe3b872f
David Eastmond <davebuck1981@gmail.com>
2023-02-06 00:59:51 -0500

    wip plaid confirm

---
+++ src/pages/signup/PlaidConfirm.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material"
+
+export default function PlaidConfirm (props) {
+
+  /* When this page loads: 
+    - Attempt to log in with the current application
+    - If an e-mail already exists, we should get an error. Inform the user to log in to their portal continue or create a new application
+    
+  
+  */
+  return (
+    <Box>
+      <p>Plaid confirm</p>
+    </Box>
+  )
+}
--- src/pages/signup/StepFour.jsx
+++ src/pages/signup/StepFour.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Typography } from '@mui/material';
-// import { PlaidLinkWidget } from '../../components/PlaidLinkWidget';
+import { Typography, Box } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { SignupValidator } from './validate-signup';
+import { SIGNUP_FIELDS } from './sign-up-fields';
 
-export default function StepFour() {
+/*
+  Here we should check that all validations are good and not let the user progress
+*/
+export default function StepFour(props) {
+  const { submitState, onInvalidState } = props;
+  const [validationErrors, setValidationErrors] = useState(null);
+  useEffect(() => {
+    const allFields = Object.keys(SIGNUP_FIELDS)
+    const errors = SignupValidator.validate(allFields, submitState);
+    if (Object.keys(errors).length > 0) {
+      onInvalidState(true);
+      setValidationErrors(errors);
+    }
+  }, [])
   return (
-    <div>
+    <Box mt={3}>
+      <Box display="flex" flexDirection={"column"} justifyContent={"center"}>
         <Typography variant="h4" component="h2" align="center">
             Instant Bank Verification (IBV)
         </Typography>
-
-        <Typography variant="body" component="p" align="center">
-            To complete your credit application, we need to perform a real-time verification of your primary bank account <br/> (active and where your paychecks are deposited)
+        <Box>
+          <p>Please click back and resolve these errors first before continuing.</p>
+        </Box>
+        <Typography  align="center">
+          To complete your credit application, we need to perform a real-time verification of your primary bank account <br/> (active and where your paychecks are deposited)
         </Typography>
-
-        {/* <div>
-            <PlaidLinkWidget />
-        </div>     */}
-        
-    </div>
+        <Typography  align="center">
+          Click next to register your file with us and connect your financial details.
+        </Typography>
+      </Box>
+    </Box>
   )
 }
