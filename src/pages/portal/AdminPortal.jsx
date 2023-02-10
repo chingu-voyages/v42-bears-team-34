ee13e7d00939a7a04fd7ef9e77c05a9685cbc8ec
Bachir <bachir@Bachirs-MacBook-Pro.local>
2023-02-09 23:43:35 -0500

    added content to admin portal

---
+++ src/pages/portal/AdminPortal.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function AdminPortal() {
+  return (
+    <div>
+      <Card sx={{ width: 800 }}>
+      <CardContent>
+        <Typography variant="h4">
+          Admin Dashboard
+        </Typography>
+        <Typography variant="h5" component="div">
+         Applications
+        </Typography>
+      </CardContent>
+    </Card>
+    </div>
+  )
+}
