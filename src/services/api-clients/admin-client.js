29cdec104ab8cb267b9b44ab96247f2daccf7383
David Eastmond <davebuck1981@gmail.com>
2023-02-11 11:48:04 -0500

    update doc strings for clients

---
+++ src/services/api-clients/admin-client.js
@@ -0,0 +1,52 @@
+import { BaseUserActionClient } from "./base-user-action-client";
+
+/**
+ * This class should handle communication to our backend
+ * for users that have a JWT
+ */
+export class AdminClient extends BaseUserActionClient {
+  constructor({ authToken }) {
+    super(
+      {
+        "Authorization": `Bearer ${authToken}`,
+      }
+    )
+  }
+ 
+  async adminGetAllApplications() {
+    return super.getData("/admin/application/all");
+  }
+
+   /**
+   * @param {string} id 
+   * @returns {Promise<{ 
+   * id: string,
+   * applicantGender: string,
+   * email: string,
+   * dateOfBirth: string,
+   * firstName: string,
+   * lastName: string,
+   * address: { streetAddress: string, unitNumber: string, city: string, additionalAddress: string, postalCode: string, province: string }}}
+   */
+  async adminGetUserById(id) {
+    return super.getData(`/auth/user/${id}`)
+  }
+
+  /**
+   * 
+   * @param {string} id applicationId 
+   * @returns {Promise<{ 
+   * requestedLoanAmount: number,
+    *  numberOfInstallments: number,
+    *  installmentAmount: number,
+    *  loanPurpose: string
+    *  status: string
+    *  requestedAt: string
+    *  requestedBy: string
+    * rejectedReason: string
+   * }>} an application document
+   */
+  async adminGetApplicationById(id) {
+    return super.getData(`/application/view/${id}`)
+  }
+}
