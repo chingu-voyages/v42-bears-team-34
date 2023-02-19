44fe42e4faf015cd737201ac2991dadcfae84c0e
David Eastmond <davebuck1981@gmail.com>
2023-02-19 09:51:00 -0500

    added new color. Application date to table

--- src/pages/admin/application-summary-table-row/ApplicationSummaryTableRow.jsx
+++ src/pages/admin/application-summary-table-row/ApplicationSummaryTableRow.jsx
@@ -1,35 +1,53 @@
-import { TableRow, TableCell, styled } from "@mui/material";
-import { useCallback } from "react";
-import { PALLET } from "../../../stylings/pallet";
-import { STRING_HELPERS } from "../../../utils/string-helpers";
-import { FIELD_DICT } from "../../signup/sign-up-fields";
+import { TableRow, TableCell, styled } from '@mui/material';
+import { useCallback } from 'react';
+import { PALLET } from '../../../stylings/pallet';
+import { STRING_HELPERS } from '../../../utils/string-helpers';
+import { FIELD_DICT } from '../../signup/sign-up-fields';
 
 const StyledTableRow = styled(TableRow)((props) => ({
-  "&:hover": {
-    cursor: "pointer",
-    backgroundColor: PALLET.mountainDewLime,
-  }
-}))
+	'&:hover': {
+		cursor: 'pointer',
+		backgroundColor: PALLET.application.summaryRow.highlightColor,
+	},
+}));
 
 /**
  * This returns summary card details. Clickable
- * @param {{ application: Object, user: Object}} props 
+ * @param {{ application: Object, user: Object}} props
  * @returns JSX.Element
  */
-export function ApplicationSummaryTableRow (props) {
-  const { application, user, onApplicationClicked } = props;
-  const handleOnClick = useCallback(() => {
-    onApplicationClicked && onApplicationClicked(application.id);
-  })
-  return (
-    <StyledTableRow onClick={handleOnClick}>
-      <TableCell align="right">{STRING_HELPERS.capitalizeFirstLetter(FIELD_DICT.applicationStatus[application?.status])}</TableCell>
-      <TableCell align="right">{user?.lastName} {user?.firstName}</TableCell>
-      <TableCell align="right">{STRING_HELPERS.capitalizeFirstLetter(user?.applicantGender)}</TableCell>
-      <TableCell align="right">{user?.address?.city?.toUpperCase()}</TableCell>
-      <TableCell align="right">{user?.address?.province?.toUpperCase()}</TableCell>
-      <TableCell align="right">{STRING_HELPERS.toCurrency(application?.requestedLoanAmount)}</TableCell>
-      <TableCell align="right">{STRING_HELPERS.toCurrency(application?.applicantIncome)}</TableCell>
-    </StyledTableRow>
-  )
+export function ApplicationSummaryTableRow(props) {
+	const { application, user, onApplicationClicked } = props;
+	const handleOnClick = useCallback(() => {
+		onApplicationClicked && onApplicationClicked(application.id);
+	});
+	return (
+		<StyledTableRow onClick={handleOnClick}>
+			<TableCell
+				sx={{ color: PALLET.application[application?.status] }}
+				align="right"
+			>
+				{STRING_HELPERS.capitalizeFirstLetter(
+					FIELD_DICT.applicationStatus[application?.status]
+				)}
+			</TableCell>
+			<TableCell>{STRING_HELPERS.formatDate(application?.createdAt)}</TableCell>
+			<TableCell align="right">
+				{user?.lastName} {user?.firstName}
+			</TableCell>
+			<TableCell align="right">
+				{STRING_HELPERS.capitalizeFirstLetter(user?.applicantGender)}
+			</TableCell>
+			<TableCell align="right">{user?.address?.city?.toUpperCase()}</TableCell>
+			<TableCell align="right">
+				{user?.address?.province?.toUpperCase()}
+			</TableCell>
+			<TableCell align="right">
+				{STRING_HELPERS.toCurrency(application?.requestedLoanAmount)}
+			</TableCell>
+			<TableCell align="right">
+				{STRING_HELPERS.toCurrency(application?.applicantIncome)}
+			</TableCell>
+		</StyledTableRow>
+	);
 }
--- src/pages/admin/applications-list/ApplicationsList.jsx
+++ src/pages/admin/applications-list/ApplicationsList.jsx
@@ -1,45 +1,71 @@
-import { styled, Paper, Table, TableContainer, TableCell, TableHead, TableBody, TableRow } from "@mui/material";
-import { useCallback } from "react";
-import { PALLET } from "../../../stylings/pallet";
-import { FIELD_DICT, SIGNUP_FIELDS } from "../../signup/sign-up-fields";
-import { ApplicationSummaryTableRow } from "../application-summary-table-row/ApplicationSummaryTableRow";
-import { headerCellStyles } from "../header-cell-style/header-cell-style";
+import {
+	styled,
+	Paper,
+	Table,
+	TableContainer,
+	TableCell,
+	TableHead,
+	TableBody,
+	TableRow,
+} from '@mui/material';
+import { useCallback } from 'react';
+import { PALLET } from '../../../stylings/pallet';
+import { FIELD_DICT, SIGNUP_FIELDS } from '../../signup/sign-up-fields';
+import { ApplicationSummaryTableRow } from '../application-summary-table-row/ApplicationSummaryTableRow';
+import { headerCellStyles } from '../header-cell-style/header-cell-style';
 
 const StyledTableHeaderCell = styled(TableCell)((props) => ({
-  ...headerCellStyles,
-}))
+	...headerCellStyles,
+}));
 
-const CustomStyledTableContainer = styled(TableContainer)((props) => ({
-  
-}))
+const CustomStyledTableContainer = styled(TableContainer)((props) => ({}));
 
-export function ApplicationsList (props) {
-  const { userApplications, onApplicationClicked } = props;
+export function ApplicationsList(props) {
+	const { userApplications, onApplicationClicked } = props;
 
-  const handleApplicationClicked = useCallback((id) => {
-    onApplicationClicked && onApplicationClicked(id);
-  })
+	const handleApplicationClicked = useCallback((id) => {
+		onApplicationClicked && onApplicationClicked(id);
+	});
 
-  return (
-    <CustomStyledTableContainer component={Paper}>
-      <Table sx={{ backgroundColor: PALLET.applicationDetails.backgroundColor}} aria-label="applications table">
-        <TableHead>
-          <TableRow>
-            <StyledTableHeaderCell>Status</StyledTableHeaderCell>
-            <StyledTableHeaderCell>Name</StyledTableHeaderCell>
-            <StyledTableHeaderCell>{FIELD_DICT[SIGNUP_FIELDS.applicantGender]}</StyledTableHeaderCell>
-            <StyledTableHeaderCell>{FIELD_DICT[SIGNUP_FIELDS.city]}</StyledTableHeaderCell>
-            <StyledTableHeaderCell>{FIELD_DICT[SIGNUP_FIELDS.province]}</StyledTableHeaderCell>
-            <StyledTableHeaderCell>{FIELD_DICT[SIGNUP_FIELDS.requestedLoanAmount]} </StyledTableHeaderCell>
-            <StyledTableHeaderCell>{FIELD_DICT[SIGNUP_FIELDS.applicantIncome]}</StyledTableHeaderCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          { userApplications && userApplications.map((app) => (
-            <ApplicationSummaryTableRow key={app.id} {...{...app}} onApplicationClicked={handleApplicationClicked} />
-          ))}
-        </TableBody>
-      </Table>
-    </CustomStyledTableContainer>
-  )
+	return (
+		<CustomStyledTableContainer component={Paper}>
+			<Table
+				sx={{ backgroundColor: PALLET.applicationDetails.backgroundColor }}
+				aria-label="applications table"
+			>
+				<TableHead>
+					<TableRow>
+						<StyledTableHeaderCell>Status</StyledTableHeaderCell>
+						<StyledTableHeaderCell>Date</StyledTableHeaderCell>
+						<StyledTableHeaderCell>Name</StyledTableHeaderCell>
+						<StyledTableHeaderCell>
+							{FIELD_DICT[SIGNUP_FIELDS.applicantGender]}
+						</StyledTableHeaderCell>
+						<StyledTableHeaderCell>
+							{FIELD_DICT[SIGNUP_FIELDS.city]}
+						</StyledTableHeaderCell>
+						<StyledTableHeaderCell>
+							{FIELD_DICT[SIGNUP_FIELDS.province]}
+						</StyledTableHeaderCell>
+						<StyledTableHeaderCell>
+							{FIELD_DICT[SIGNUP_FIELDS.requestedLoanAmount]}{' '}
+						</StyledTableHeaderCell>
+						<StyledTableHeaderCell>
+							{FIELD_DICT[SIGNUP_FIELDS.applicantIncome]}
+						</StyledTableHeaderCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{userApplications &&
+						userApplications.map((app) => (
+							<ApplicationSummaryTableRow
+								key={app.id}
+								{...{ ...app }}
+								onApplicationClicked={handleApplicationClicked}
+							/>
+						))}
+				</TableBody>
+			</Table>
+		</CustomStyledTableContainer>
+	);
 }
