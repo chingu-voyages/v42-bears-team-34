b1af3cd56da15c8c2509088819f07b93113a04bc
David Eastmond <davebuck1981@gmail.com>
2023-02-19 08:46:04 -0500

    admin control adjustments

--- src/components/ApplicationViewContainer/resources/AdminControlsComponent.jsx
+++ src/components/ApplicationViewContainer/resources/AdminControlsComponent.jsx
@@ -1,7 +1,7 @@
-import { Box } from "@mui/material";
-import { useCallback } from "react";
-import { PALLET } from "../../../stylings/pallet";
-import StyledButton from "../../StyledButton/StyledButton";
+import { Box } from '@mui/material';
+import { useCallback } from 'react';
+import { PALLET } from '../../../stylings/pallet';
+import StyledButton from '../../StyledButton/StyledButton';
 
 /**
  * Admin controls, fetch plaid financial, approve decline
@@ -11,73 +11,77 @@ import StyledButton from "../../StyledButton/StyledButton";
  * fetchButtonDisabled?: boolean
  * approveButtonDisabled?: boolean
  * rejectButtonDisabled?: boolean
- *  }} props 
- * @returns 
+ * adminOptionsButtonDisabled?: boolean
+ *  }} props
+ * @returns
  */
 export function AdminControlsComponent(props) {
-  const { onFetchFinancialDataClick, 
-          onApproveClick, 
-          onRejectClick,
-          onAdminOptionsClick,
-          fetchButtonDisabled,
-          approveButtonDisabled,
-          rejectButtonDisabled
-        } = props;
+	const {
+		onFetchFinancialDataClick,
+		onApproveClick,
+		onRejectClick,
+		onAdminOptionsClick,
+		fetchButtonDisabled,
+		approveButtonDisabled,
+		rejectButtonDisabled,
+		adminOptionsButtonDisabled,
+	} = props;
 
-  const handleFetchFinancialDataClick = useCallback(() => {
-    onFetchFinancialDataClick && onFetchFinancialDataClick();
-  });
+	const handleFetchFinancialDataClick = useCallback(() => {
+		onFetchFinancialDataClick && onFetchFinancialDataClick();
+	});
 
-  const handleApproveClick = useCallback(() => {
-    onApproveClick && onApproveClick();
-  })
+	const handleApproveClick = useCallback(() => {
+		onApproveClick && onApproveClick();
+	});
 
-  const handleRejectedClick = useCallback(() => {
-    onRejectClick && onRejectClick();
-  })
+	const handleRejectedClick = useCallback(() => {
+		onRejectClick && onRejectClick();
+	});
 
-  const handleMoreAdminOptionsClicked = useCallback(() => {
-    onAdminOptionsClick && onAdminOptionsClick()
-  })
+	const handleMoreAdminOptionsClicked = useCallback(() => {
+		onAdminOptionsClick && onAdminOptionsClick();
+	});
 
-  return (
-    <Box display="flex" justifyContent={"center"} pl={2} pr={2}>
-      <Box>
-        <Box p={2} display={"flex"} justifyContent={"center"}>
-          <StyledButton
-            borderRadius="20px"
-            buttonColor={PALLET.mountainDewLime}
-            label={"Get Financial Details"}
-            onClick={handleFetchFinancialDataClick}
-            disabled={fetchButtonDisabled}
-          />
-        </Box>
-        <Box display="flex" p={2} justifyContent={"space-evenly"}>
-          <StyledButton 
-            borderRadius="20px"
-            buttonTextColor={PALLET.white}
-            buttonColor={PALLET.application.approved}
-            onClick={handleApproveClick}
-            label={"Approve"}
-            disabled={approveButtonDisabled}
-          />
-          <StyledButton
-            borderRadius="20px"
-            buttonTextColor={PALLET.white}
-            buttonColor={PALLET.application.rejected}
-            label={"Reject"}
-            disabled={rejectButtonDisabled}
-            onClick={handleRejectedClick}
-          />
-          <StyledButton 
-            borderRadius="20px"
-            buttonTextColor={PALLET.white}
-            buttonColor={PALLET.pineGreen}
-            label={"More Admin Options..."}
-            onClick={handleMoreAdminOptionsClicked}
-          />
-        </Box>
-      </Box>
-    </Box>
-  )
+	return (
+		<Box display="flex" justifyContent={'center'} pl={2} pr={2}>
+			<Box>
+				<Box p={2} display={'flex'} justifyContent={'center'}>
+					<StyledButton
+						borderRadius="20px"
+						buttonColor={PALLET.mountainDewLime}
+						label={'Get Financial Details'}
+						onClick={handleFetchFinancialDataClick}
+						disabled={fetchButtonDisabled}
+					/>
+				</Box>
+				<Box display="flex" p={2} justifyContent={'space-evenly'}>
+					<StyledButton
+						borderRadius="20px"
+						buttonTextColor={PALLET.white}
+						buttonColor={PALLET.application.approved}
+						onClick={handleApproveClick}
+						label={'Approve'}
+						disabled={approveButtonDisabled}
+					/>
+					<StyledButton
+						borderRadius="20px"
+						buttonTextColor={PALLET.white}
+						buttonColor={PALLET.application.rejected}
+						label={'Reject'}
+						disabled={rejectButtonDisabled}
+						onClick={handleRejectedClick}
+					/>
+					<StyledButton
+						borderRadius="20px"
+						buttonTextColor={PALLET.white}
+						buttonColor={PALLET.pineGreen}
+						label={'More Admin Options...'}
+						disabled={adminOptionsButtonDisabled}
+						onClick={handleMoreAdminOptionsClicked}
+					/>
+				</Box>
+			</Box>
+		</Box>
+	);
 }
