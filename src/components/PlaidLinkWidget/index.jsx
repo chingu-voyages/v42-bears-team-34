52eab976b563a8c15cbbc8422767daa5feccff0f
David Eastmond <davebuck1981@gmail.com>
2023-02-27 21:02:02 -0500

    adjust import

--- src/components/PlaidLinkWidget/index.jsx
+++ src/components/PlaidLinkWidget/index.jsx
@@ -1,3 +1,3 @@
-import PlaidLinkWidget from './Plaid-link-widget';
+
 import PlaidLinkContainer from './PlaidLinkContainer';
-export { PlaidLinkWidget, PlaidLinkContainer };
+export { PlaidLinkContainer };
