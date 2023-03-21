31042ebb3214a14bded10f9ab6bf972ad15774e3
David Eastmond <davebuck1981@gmail.com>
2023-03-20 21:23:24 -0400

    factor our getUserProfile function

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,24 +1,9 @@
-import { Box, styled } from '@mui/material';
 import React, { useCallback, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoginComponent } from '../../components/LoginComponent';
-import { APP_ACTIONS } from '../../context/app.actions';
 import AppContext from '../../context/AppContext';
-import { UserClient } from '../../services/api-clients/user-client';
-import { TokenManager } from '../../services/token-manager/token-manager';
-
-const ResponsiveParentContainer = styled(Box)((props) => ({
-  marginTop: '50px',
-  [props.theme.breakpoints.up('md')]: {
-    marginTop: '200px',
-    marginLeft: '5rem',
-    marginRight: '5rem',
-  },
-  [props.theme.breakpoints.up('lg')]: {
-    marginLeft: '30vw',
-    marginRight: '30vw',
-  },
-}));
+import { ResponsiveParentContainer } from '../../components/ResponsiveParentContainer/ResponsiveParentContainer';
+import { getUserProfile } from '../../services/get-user-profile/get-user-profile';
 /**
  * Generic login page for use with admin or user
  * @returns JSX.Element
@@ -26,73 +11,20 @@ const ResponsiveParentContainer = styled(Box)((props) => ({
 function LoginPage(props) {
   const navigate = useNavigate();
   const { dispatch } = useContext(AppContext);
-
+  const { isAdmin, passwordResetMode } = props;
   const handleOnLoginSuccess = useCallback(() => {
     /* Handle log in. 
-    The token should be written at the LoginComponent level
     Redirect the user to appropriate page on success depending on admin status
     */
-
-    const getUserProfile = async () => {
-      try {
-        const authToken = TokenManager.getToken();
-        const userClient = new UserClient({ authToken });
-        const userProfile = await userClient.getUserProfile();
-        if (userProfile) {
-          const { id, firstName, lastName, role, iat, exp, expired } =
-            userProfile;
-          // Update state
-          dispatch({
-            type: APP_ACTIONS.SET_STATE,
-            state: {
-              isAuthenticated: true,
-              user: {
-                id,
-                firstName,
-                lastName,
-                role,
-                iat,
-                exp,
-                expired,
-              },
-            },
-          });
-          // If the user's role is admin, redirect them to admin landing page
-          if (role === 'admin' && props.isAdmin) {
-            navigate('/admin/applications', { replace: true });
-          } else {
-            /* Direct them to normal user landing page 
-          where user can view their pending application and status
-          */
-            navigate(`/user/applications`, { replace: true });
-          }
-        }
-      } catch (exception) {
-        // If there's some error. Update state.
-        console.error(exception);
-        dispatch({
-          type: APP_ACTIONS.SET_STATE,
-          state: {
-            isAuthenticated: false,
-            user: {
-              id: null,
-              firstName: null,
-              lastName: null,
-              email: null,
-              role: 'user',
-            },
-          },
-        });
-      }
-    };
-    getUserProfile();
+    getUserProfile(dispatch, navigate, isAdmin);
   }, []);
 
   return (
     <ResponsiveParentContainer>
       <LoginComponent
-        isAdmin={props.isAdmin}
+        isAdmin={isAdmin}
         onLoginSuccess={handleOnLoginSuccess}
+        passwordResetMode={passwordResetMode}
       />
     </ResponsiveParentContainer>
   );
