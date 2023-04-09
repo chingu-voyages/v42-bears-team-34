04825904453930e355cad89d663f6329712379b6
David Eastmond <davebuck1981@gmail.com>
2023-04-09 08:59:05 -0400

    create hook for verifying if e-mail is confirmed/verified

---
+++ src/hooks/UserGetIsEmailVerified.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import { AuthClient } from '../services/api-clients/auth-client';
+
+// This is a hook to check if an e-mail address is verified
+export function useIsEmailVerified(email) {
+  const [isEmailVerified, setIsEmailVerified] = useState(false);
+  useEffect(() => {
+    const checkEmailVerified = async () => {
+      try {
+        if (email) {
+          const authClient = new AuthClient();
+          const result = await authClient.getIsEmailVerified(email);
+          setIsEmailVerified(result.value);
+        }
+      } catch (err) {
+        console.error(err);
+        setIsEmailVerified(false);
+      }
+    };
+    checkEmailVerified();
+  }, [email]);
+
+  return [isEmailVerified];
+}
