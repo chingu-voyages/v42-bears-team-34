d2d9bf6e648c8cf40faae308e1d6c2ff94df9814
David Eastmond <davebuck1981@gmail.com>
2023-04-09 08:56:28 -0400

    add client actions

--- src/services/api-clients/auth-client.js
+++ src/services/api-clients/auth-client.js
@@ -82,7 +82,8 @@ export class AuthClient extends BaseClient {
 
   /**
    *
-   * @param { email: string} email
+   * @param {string} email
+   * @param {boolean} resend
    */
   async triggerVerificationCodeEmail(email) {
     return super.postData('/auth/verification-code-email', { email: email });
@@ -95,5 +96,15 @@ export class AuthClient extends BaseClient {
    */
   async getIsEmailVerified(email) {
     return super.getData(`/auth/isEmailVerified/${email}`);
+  }
+
+  /**
+   *
+   * @param {string} email
+   * @param {string} code
+   * @returns {Promise<{ result: boolean, msg: string}>} the result of the operation
+   */
+  async verifyEmail(email, code) {
+    return super.postData('/auth/email/verify', { email: email, code: code });
   }
 }
